37ffdd7f4a416274026e420f87708a26f8853ed0
Frederico-Cassola-Dev <cfcassola@gmail.com>
2026-03-02 10:49:54 +0100

    Resolve rebase conflicts

---
+++ nvim/.config/nvim/lua/plugins/php.lua
@@ -0,0 +1,83 @@
+return {
+  -- Configure Mason
+  {
+    "mason-org/mason.nvim",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "phpactor",
+        "php-cs-fixer",
+        "twig-cs-fixer",
+        "phpstan",
+        "php-debug-adapter",
+      })
+    end,
+  },
+
+  -- Configure LSP for PHP and Twig
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        intelephense = {
+          -- settings for intelephense
+          settings = {
+            intelephense = {
+              completion = {
+                fullyQualifyImportedSymbols = true,
+              },
+              diagnostics = {
+                enable = true,
+              },
+            },
+          },
+        },
+        phpactor = {
+          -- settings for phpactor
+          settings = {
+            phpactor = {
+              language_server_php_cs_fixer = {
+                enabled = true,
+              },
+              language_server_phpstan = {
+                enabled = true,
+              },
+            },
+          },
+        },
+        twigls = {},
+      },
+    },
+  },
+
+
+
+  -- Configure formatting
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        php = { "php-cs-fixer" },
+        twig = { "twig-cs-fixer" },
+      },
+      formatters = {
+        ["php-cs-fixer"] = {
+          command = "php-cs-fixer",
+          args = {
+            "fix",
+            "$FILENAME",
+            "--rules=@Symfony", -- Use Symfony coding standards
+          },
+          stdin = false,
+        },
+        ["twig-cs-fixer"] = {
+          command = "twig-cs-fixer",
+          args = {
+            "fix",
+            "$FILENAME",
+          },
+          stdin = false,
+        },
+      },
+    },
+  },
+}
